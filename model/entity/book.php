43d2b79af3ada9d909ba0881c8121cd9f1a54f02
Baptiste COLLIGNON <collignon@CDI>
2020-10-27 12:08:29 +0100

    Book & User get/set method done

--- model/entity/book.php
+++ model/entity/book.php
@@ -1,6 +1,77 @@
 <?php
 // Classe représetant les livres stockés en base de données
-class Livre {
+class Book {
 
+    protected int $id;
+    protected string $tittle;
+    protected string $author;
+    protected string $resume;
+    protected string $publication_date;
+    protected string $category;
+    protected bool $status;
 
+    protected USER $user;
+
+    public function getId():int{
+        return $this->id;
+    }
+    public function getTittle():string{
+        return $this->tittle;
+    }
+    public function getAuthor():string{
+        return $this->author;
+    }
+    public function getResume():string{
+        return $this->resume;
+    }
+    public function getPublication_date():string{
+        return $this->publication_date;
+    }
+    public function getCategory():string{
+        return $this->category;
+    }
+    public function getStatus():string{
+        return $this->status;
+    }
+    public function getUser():User{
+        return $this->user;
+    }
+
+    public function setId(int $id){
+        $this->id = $id;
+    }
+    public function setTittle(string $tittle){
+        $this->tittle = $tittle;
+    }
+    public function setAuthor(string $author){
+        $this->author = $author;
+    }
+    public function setResume(string $resume){
+        $this->resume = $resume;
+    }
+    public function setPublication_date(string $publication){
+        $this->publication_date = $publication;
+    }
+    public function setCategory(string $category){
+        $this->category = $category;
+    }
+    public function setStatus(bool $status){
+        $this->status = $status;
+    }
+    public function setUser(User $user){
+        $this->user = $user;
+    }
+
+    private function hydrate(array $datas){
+        foreach($datas as $key => $value){
+            $method = "set" ; ucfirst($key);
+            if (method_exists($this, $method)){
+                $this->$method($value);
+            }
+        }
+    }
+
+    public function __construct(array $data){
+        $this->hydrate($data);
+    }
 }
--- model/entity/user.php
+++ model/entity/user.php
@@ -1,6 +1,68 @@
 <?php
 // Classe représetant les utilisateurs stockés en base de données
-class Utilisateur {
+class User {
 
+    const SEX = ["male", "female"];
+
+    protected int $id;
+    protected string $firstname;
+    protected string $lastname;
+    protected string $sex;
+    protected string $birthdate;
+    protected string $address;
+    protected string $postal_code;
+    protected string $city;
+
+    public function getId():int {
+        return $this->id;
+    }
+    public function getFirstname():string{
+        return $this->firstname;
+    }
+    public function getLastname():string{
+        return $this->lastname;
+    }
+    public function getSex():string{
+        return $this->sex;
+    }
+    public function getBirthdate():string{
+        return $this->birthdate;
+    }
+    public function getAddress():string{
+        return $this->address;
+    }
+    public function getPostal_code():string{
+        return $this->postal_code;
+    }
+    public function getCity():string{
+        return $this->ciy;
+    }
+
+    public function setId(int $id){
+        $this->id = $id;
+    }
+    public function setLastname(string $lastname){
+        $this->lastname = $lastname;
+    }
+    public function setFirdtname(string $firstname){
+        $this->firstname = $firstname;
+    }
+    public function setSex(string $sex){
+        if (in_array($sex, self::SEX)){
+            $this->sex = $sex;
+        }
+    }
+    public function setBirthdate(string $date){
+        $this->birthdate= $date;
+    }
+    public function setAddress(string $address){
+        $this->address = $address;
+    }
+    public function setPostal_code(string $pCode){
+        $this->postal_code = $pCode;
+    }
+    public function setCity(string $city){
+        $this->city = $city;
+    }
 
 }
